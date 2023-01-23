7046c7920c6ca306289a211cfa919b1940bcb99b
N-Laboratory <xnbaxx@gmail.com>
2023-01-23 20:48:08 +0900

    feat: add index.vue to pages

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import BaseTextInput from '../components/atoms/BaseTextInput.vue'
+const text = ref('')
+</script>
+
+<template>
+  <div>
+    <h1>Pages/index.vue</h1>
+    <BaseTextInput v-model="text" />
+    <p>Input value = {{ text }}</p>
+  </div>
+</template>
+
+<style scoped></style>
