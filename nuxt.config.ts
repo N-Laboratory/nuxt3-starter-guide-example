29e22e0c070a9811d9b4531bd8e634d2f35252fe
N-Laboratory <xnbaxx@gmail.com>
2023-01-23 20:45:09 +0900

    refactor: set srcDir in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'src/',
   typescript: {
     shim: false,
     strict: true,
     typeCheck: true
-  },
+  }
 })
