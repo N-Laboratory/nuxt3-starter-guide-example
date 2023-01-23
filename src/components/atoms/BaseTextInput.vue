d323dce05f20fc6ccfc6e4c28001c7195a21251d
N-Laboratory <xnbaxx@gmail.com>
2023-01-23 20:47:59 +0900

    feat: add BaseTextInput.vue to components/atoms

---
+++ src/components/atoms/BaseTextInput.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+interface Props {
+  modelValue: string
+}
+interface Emits {
+  (e: 'update:modelValue', newValue: string): void
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const value = computed({
+  get (): string {
+    return props.modelValue
+  },
+  set (value: string) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+
+<template>
+  <input v-model="value" type="text">
+</template>
+
+<style scoped></style>
