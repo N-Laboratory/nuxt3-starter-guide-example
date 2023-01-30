dd671a7429326f4a08ff3f48714508411640133e
N-Laboratory <xnbaxx@gmail.com>
2023-01-30 22:06:26 +0900

    fix: fix myPage navigation guard rules

--- src/middleware/redirect.global.ts
+++ src/middleware/redirect.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.path === '/' || to.path === '/myPage') {
+  if (to.path === '/' || (to.path === '/myPage' && from.path === '/myPage')) {
     return navigateTo('formScript')
   }
 })
